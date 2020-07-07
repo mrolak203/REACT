202f6cf77ff5527957072a939bf91d935eed3c7f
Michelle Rolak <michellerolak7@gmail.com>
2020-07-07 13:58:53 -0400

    numbers display

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,3 +1,20 @@
-/*
- * Implement all your JavaScript in this file!
- */
+
+var display = '';
+
+$(function() {
+
+    $(".digit").click(function() {
+   
+    	display += $(this).val();
+    	$("#display").val(Number(display));
+
+ 	});
+
+ 	$("#clearButton").click(function (){
+
+ 		display = '';
+ 		$("#display").val(display);
+
+ 	});
+ 	
+});
