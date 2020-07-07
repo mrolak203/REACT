8c1442b5b78ae509de74dfcf20dc56a3b7ec3d35
Michelle Rolak <michellerolak7@gmail.com>
2020-07-07 19:07:21 -0400

    display toolbar on click

--- TextEdit/FontChooser.js
+++ TextEdit/FontChooser.js
@@ -2,20 +2,39 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
+	this.state = { isHidden: true};
     }
     
+    //when the text is clicked, change the visibility of all components to their inverse 
+    clickText(){
+
+    	
+    	 this.setState({isHidden : !this.state.isHidden});
+
+    	 var tools = document.getElementsByClassName("tool");
+    	
+    	 for (let tool of tools){
+    	 	tool.hidden = this.state.isHidden;
+    	 }
+
+
+
+    }
 
     render() {
 
 	return(
+
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
+	       <input type="checkbox" className = "tool"  id="boldCheckbox" hidden={true}/>
+	       <button id="decreaseButton" className = "tool" hidden={true}>-</button>
+	       <span id="fontSizeSpan" className = "tool" hidden={true}>{this.props.size}</span>
+	       <button id="increaseButton" className = "tool" hidden={true}>+</button>
+	       <span id="textSpan"
+					onClick={this.clickText.bind(this)}>{this.props.text} </span>
 	       </div>
 	);
+
     }
 }
 
