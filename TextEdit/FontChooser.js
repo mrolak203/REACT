17f33459515c0c250dd0416fad69504ee8e6dcd5
Michelle Rolak <michellerolak7@gmail.com>
2020-07-07 19:17:11 -0400

    add font weight edit functionality

--- TextEdit/FontChooser.js
+++ TextEdit/FontChooser.js
@@ -2,13 +2,12 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state = { isHidden: true};
+	this.state = { isHidden: true, bold: !this.props.bold};
     }
     
     //when the text is clicked, change the visibility of all components to their inverse 
     clickText(){
 
-    	
     	 this.setState({isHidden : !this.state.isHidden});
 
     	 var tools = document.getElementsByClassName("tool");
@@ -16,21 +15,28 @@ class FontChooser extends React.Component {
     	 for (let tool of tools){
     	 	tool.hidden = this.state.isHidden;
     	 }
+    }
 
+    //when the checkbox is clicked, the fontWeight changes from bold or becomes normal
+    clickCheckBox(){
 
+    	this.setState({bold: !this.state.bold});
 
     }
 
     render() {
 
+    		var weight = this.state.bold ? 'bold' : 'normal';
+
 	return(
 
 	       <div>
-	       <input type="checkbox" className = "tool"  id="boldCheckbox" hidden={true}/>
+	       <input type="checkbox" className = "tool"  id="boldCheckbox" hidden={true}
+	        onClick={this.clickCheckBox.bind(this)}/>
 	       <button id="decreaseButton" className = "tool" hidden={true}>-</button>
 	       <span id="fontSizeSpan" className = "tool" hidden={true}>{this.props.size}</span>
 	       <button id="increaseButton" className = "tool" hidden={true}>+</button>
-	       <span id="textSpan"
+	       <span id="textSpan" style = {{ fontWeight: weight }}
 					onClick={this.clickText.bind(this)}>{this.props.text} </span>
 	       </div>
 	);
