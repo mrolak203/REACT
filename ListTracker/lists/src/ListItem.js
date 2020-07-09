e0b6936ab7befdbe998a2d021eb04bdcf0a9659d
Michelle Rolak <michellerolak7@gmail.com>
2020-07-09 13:42:44 -0400

    change color upon clicking on item

--- ListTracker/lists/src/ListItem.js
+++ ListTracker/lists/src/ListItem.js
@@ -8,7 +8,9 @@ class ListItem extends Component {
     }
 
     handleClick() {
-	// Implement this function!
+
+      this.setState({color: (this.state.color == 'black')? 'grey': 'black' });
+
     }
 
   render() {
